1f6b7f0e837a57f8ede1eec117853110b23de5fb
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-23 06:04:41 -0400

    countdown always 10 days

--- app.js
+++ app.js
@@ -26,7 +26,14 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".giveaway");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2025, 4, 24, 11, 30, 0);
+let temDate = new Date();
+let tempYear = temDate.getFullYear();
+let tempMonth = temDate.getMonth();
+let tempDay = temDate.getDate();
+
+// let futureDate = new Date(2025, 4, 24, 11, 30, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
